592729233169c6fd2eb66c4e8bf270e0c0a8729a
Brian Van Stone <vanstone_b@subway.com>
2018-01-15 13:37:14 -0500

    Added note model

--- go/src/main/java/tech/notpaper/go/GoApplication.java
+++ go/src/main/java/tech/notpaper/go/GoApplication.java
@@ -2,8 +2,10 @@ package tech.notpaper.go;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GoApplication {
 
 	public static void main(String[] args) {
